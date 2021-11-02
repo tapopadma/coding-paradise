6e34a26f63600ced4329801831a61f5c30dd43a1
tapopadma <tapopadmatripathy1995@gmail.com>
2021-11-02 12:56:22 +0530

    adding readme and skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+Provides C++ library for various data structures and algorithms. No new invention is involved here, but this certainly comes handy during programming competitions and/or preparation for coding interviews.
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 02-Nov-2021
+ *      Author: tapopadma
+ */
+
+/*
+ * main.cpp
+ *
+ *  Created on: 17-Oct-2021
+ *      Author: tapopadma
+ */
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<long long int> vll;
+typedef vector<char> vc;
+typedef pair<int, int> pi;
+typedef pair<long long int, long long int> pll;
+
+#define mp make_pair
+#define pb push_back
+#define el putchar('\n')
+#define sp putchar(' ')
+#define Fill(a, val) for(long unsigned int i=0;i<sizeof(a)/sizeof(a[0]);++i)a[i]=val
+#define all(a) a.begin(), a.end()
+#define tr(a, it) for (auto it = a.begin(); it != a.end(); ++it)
+#define inl(n) scanf("%lld", &n)
+#define in(n) scanf("%d", &n)
+#define out(n) printf("%d", n);
+#define outl(n) printf("%lld", n);
+#define outs(s) printf("%s", s.c_str());
+#define pf(s) printf(s)
+
+#define INF64 9223372036854775807LL
+#define INF 2147483647
+#define mod 998244353
+#define NN 600005
+
+void solve() {
+
+}
+
+int main() {
+//	auto start = chrono::high_resolution_clock::now();
+//	ios::sync_with_stdio(false);
+	solve();
+//	auto stop = chrono::high_resolution_clock::now();
+//	auto duration=chrono::duration_cast<chrono::milliseconds>(stop-start);
+//	out(duration.count());
+	return 0;
+}
+
+
+
